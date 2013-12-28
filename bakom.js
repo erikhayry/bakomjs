e1c5e0888345b1dd0c3420944e50efc40baf08ea
Erik Portin <erikportin@gmail.com>
2013-12-28 15:44:24 +0000

    Added error and warning messages, added support for bg position fixed

--- bakom.js
+++ bakom.js
@@ -23,16 +23,124 @@ window.Bakom = function(configure){
 
   		//set default values
   		bakom.defaults = {
-			backgroundSelector : '.bakom-bg-1',
-			textSelector : '.bakom-fg-1',
-			styleClass : 'text',
-			dy : '0.9em' ////https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/dy
+			backgroundSelector : 'body',
+			textSelector : '',
+			styleClass : '',
+			dy : '', //https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/dy
+			backgroundClipSupportOnly : true
 		};
 
 		//functions global to bakom
-
+		
 		//inital setup
-		var setup = function(configure){
+		setup = function(configure){
+			//get background element properties
+			var _getBackground = function(){
+
+				var _getBackgroundImageProperties = function(){
+					/*
+						TODO: add support for position fixed, local and multiple images
+					*/
+			    		var _src = '',
+			    			_xy = '',
+			    			_size = [];
+
+			        	_src = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-image'),
+			        	_backgroundAttachment = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-attachment');
+
+			        	if(!bakom.defaults.backgroundClipSupportOnly){
+			        		var _backgroundRepeat = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-repeat');
+			        		if(_backgroundRepeat !== 'no-repeat'){
+			        			console.warn('BAKOM.JS: background-repeat is set to "' + _backgroundRepeat + '" which can cause issues for browsers not supporting Background Clip. Set property to "no-repeat" to avoid this warning.')
+			        		}
+
+			        		if(_backgroundAttachment !== 'scroll'){
+			        			console.warn('BAKOM.JS: background-attachment is set to "' + _backgroundAttachment + '" which is not supported. Set property to "scroll" to avoid this warning.')
+			        		}
+			        	}
+				     
+				        if(_src && _src !== 'none') {
+				        	_xy = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-position').split(' ');
+			        		_size = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-size').split(' ');
+				        	_src = _src.slice(_src.indexOf('url(') + 4, _src.lastIndexOf(')'));
+
+				        	bgProp.pos = _getBackgroundBoxPosition();
+
+				        	if(_backgroundAttachment === 'fixed'){
+				        		bgProp.pos.left = 0;
+				        		bgProp.pos.top = 0;
+				        	}
+
+					       	bgProp.prop = {
+					        	src : _src,
+					        	x : parseInt(_xy[0], 10),
+					        	y : parseInt(_xy[1], 10),
+					        	size : {
+				        			width : parseInt(_size[0], 10), 
+				        			height : parseInt(_size[1], 10)
+					        		}
+					        }
+					        return true;
+				        }
+
+				        else {				    
+				        	console.error('BAKOM.JS: Unable to find a background image for ' + bgProp.element);
+				        	return false;
+				        }
+		    		},
+				
+					_getBackgroundBoxPosition = function(){
+						var _pos = bgProp.element.getBoundingClientRect();
+						return {
+							bottom: bgProp.element.getBoundingClientRect().bottom,
+							height: bgProp.element.getBoundingClientRect().height,
+							left: bgProp.element.getBoundingClientRect().left,
+							right: bgProp.element.getBoundingClientRect().right,
+							top: bgProp.element.getBoundingClientRect().top,
+							width: bgProp.element.getBoundingClientRect().width
+						};
+					};
+
+				if(!bakom.defaults.backgroundSelector) {
+					console.error('BAKOM.JS: Background element selector not set');
+					return false;
+				}
+				
+				bgProp.element = document.querySelectorAll(bakom.defaults.backgroundSelector)[0];
+				
+				if(bgProp.element){
+					 return _getBackgroundImageProperties();
+				}
+
+				else{
+					console.error('BAKOM.JS: Unable to find background element ' + bakom.defaults.backgroundSelector)
+					return false;
+				}
+			},
+
+			//get text element properties
+			_getText = function(){
+				if(!bakom.defaults.textSelector) {
+					console.error('BAKOM.JS: Text element selector not set' + bakom.defaults.textSelector);
+					return false;
+				}
+
+				var _element = document.querySelectorAll(bakom.defaults.textSelector)[0];
+				
+				if(_element){
+					textEl = {
+						element : _element,
+						pos : _element.getBoundingClientRect()
+					}					
+					if(!bakom.defaults.styleClass) bakom.defaults.styleClass = _element.className.slice(0);
+					return true;
+				}
+
+				else{
+					console.error('BAKOM.JS: Unable to find text element ' + bakom.defaults.textSelector);
+					return false;
+				}
+			}
 
 			//update defaults
 		  	for (var attrname in configure) {
@@ -48,68 +156,7 @@ window.Bakom = function(configure){
 		  	}
 		  	clipPathId = 'bakom-cp-' + _i;
 
-		  	//check if supports backgroundClip
-		  	var _testEl = document.createElement( "x-test" );
-		  	hasCssSupport = typeof _testEl.style.webkitBackgroundClip !== "undefined" && ( _testEl.style.webkitBackgroundClip = "text", _testEl.style.webkitBackgroundClip === "text" );
-		  	
-		},
-
-		//get background element properties
-		getBackground = function(){
-			var _getBackgroundImageProperties = function(){
-		    		var _src = '',
-		    			_xy = '',
-		    			_size = [];
-
-		        	_src = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-image');
-		        	_xy = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-position').split(' ');
-		        	_size = document.defaultView.getComputedStyle(bgProp.element, null).getPropertyValue('background-size').split(' ');
-			        
-			        if(_src) _src = _src.slice(_src.indexOf('url(') + 4, _src.lastIndexOf(')'));
-			        else console.error('Unable to find a background image for ' + bgProp.element)
-
-			        return {
-			        	src : _src,
-			        	x : parseInt(_xy[0], 10),
-			        	y : parseInt(_xy[1], 10),
-			        	size : {
-			        			width : parseInt(_size[0], 10), 
-			        			height : parseInt(_size[1], 10)
-			        		}
-			        }
-	    		},
-			
-				_getBackgroundBoxPosition = function(){
-					var _pos = bgProp.element.getBoundingClientRect();
-					return _pos;
-				};
-
-			bgProp.element = document.querySelectorAll(bakom.defaults.backgroundSelector)[0];
-			
-			if(bgProp.element){
-				bgProp.prop = _getBackgroundImageProperties(),
-				bgProp.pos = _getBackgroundBoxPosition();
-			}
-
-			else{
-				console.error('Unable to find background element ' + bakom.defaults.backgroundSelector)
-			}
-		},
-
-		//get text element properties
-		getText = function(){
-			var _element = document.querySelectorAll(bakom.defaults.textSelector)[0];
-			
-			if(_element){
-				textEl = {
-					element : _element,
-					pos : _element.getBoundingClientRect()
-				}
-			}
-
-			else{
-				console.error('Unable to find text element ' + bakom.defaults.textSelector)
-			}
+		  	return _getBackground() && _getText();		  	
 		},
 
 		//build the svgs (image and clip path)
@@ -165,11 +212,14 @@ window.Bakom = function(configure){
 			textEl.style.backgroundPosition = textEl.element.style.backgroundPosition;
 			textEl.style.backgroundImage = textEl.element.style.backgroundImage;
 			textEl.style.webkitBackgroundClip = textEl.element.style.webkitBackgroundClip;
+			textEl.style.backgroundClip = textEl.element.style.backgroundClip;
 			textEl.style.color = textEl.element.style.color;
 
 			textEl.element.style.backgroundPosition = (bgProp.pos.left - textEl.pos.left + bgProp.prop.x) + 'px ' + (bgProp.pos.top - textEl.pos.top + bgProp.prop.y) + 'px';
+			textEl.element.style.backgroundSize = bgProp.prop.size.width + 'px ' + bgProp.prop.size.height + 'px';
 			textEl.element.style.backgroundImage = 'url(' + bgProp.prop.src + ')';
 			textEl.element.style.webkitBackgroundClip = 'text';
+			textEl.element.style.backgroundClip = 'text';
 			textEl.element.style.color = 'rgba(0, 0, 0, 0)';
 			hasBeenDrawn = true;
 		},
@@ -195,11 +245,14 @@ window.Bakom = function(configure){
 
 		//setup elements
 		init = function(configure){
-			setup(configure);
-			getBackground();
-			getText();
-			if(hasCssSupport) setCSS();
-			else buildSvg();
+			if(setup(configure)){
+				if(hasCssSupport) setCSS();
+				else if(!bakom.defaults.backgroundClipSupportOnly) buildSvg();				
+			}
+			else{
+				console.error('BAKOM.JS: Something went wrong in the setup. Either the background or text element selector wasn\'t set or one of the element wasn\'t found');
+			}
+
 		},
 
 		//reset the elements to it's inital state
@@ -213,7 +266,13 @@ window.Bakom = function(configure){
 			}
 		};
 
-		init(configure);
+		//check for background clip support
+		var	_testEl = document.createElement( "x-test" ),
+		hasCssSupport = typeof _testEl.style.webkitBackgroundClip !== "undefined" && ( _testEl.style.webkitBackgroundClip = "text", _testEl.style.webkitBackgroundClip === "text" ) ||
+						typeof _testEl.style.backgroundClip !== "undefined" && ( _testEl.style.backgroundClip = "text", _testEl.style.backgroundClip === "text" )	
+
+		//initalize	
+		if((configure.backgroundClipSupportOnly && hasCssSupport) || !configure.backgroundClipSupportOnly) init(configure);
 
 	/*
 		global api
